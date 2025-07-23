7187dd181b5e91c902bc93a3af71b19e1be437d4
MuushyRoom <ronancuaresma10@gmail.com>
2025-07-23 22:28:28 +0800

    new layout ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,90 @@
 import { createContext, useReducer} from "react";
 import "./Styles/App.scss";
 import InputBar from "./Components/InputBar";
-import TodosList from "./Components/TodosList";
+// import TodosList from "./Components/TodosList";
 
-export const ACTIONS = {
-  ADD_TODO: "add-todo",
-  DELETE_TODO: "delete-todo",
-  TOGGLE_AS_DONE: "done-todo",
-  ADD_TO_CATEGORY_TODO: "add-to-category",
-  EDIT_TODO: "edit-todo",
-  CLEAR_TODO: "clear-todo",
-};
-
-
-function reducer(todos, action) {
-  switch (action.type) {
-
-    case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.inputText)];
-      break;
-    case ACTIONS.TOGGLE_AS_DONE:
-      return todos.map((todo) => {
-        if (action.payload.id === todo.id) {
-          return { ...todo, isComplete: !todo.isComplete };
-        }
-        return todo;
-      });
-      break;
-    case ACTIONS.DELETE_TODO:
-      return todos.filter((todo) => {
-        return todo.id !== action.payload.id;
-      });
-      break;
-      case ACTIONS.CLEAR_TODO:
-        return []
-      break;
-      case ACTIONS.EDIT_TODO:
-        console.log(action.payload.id)
-      break;
-        break;
-  }
-}
-
-function newTodo(user_input) {
-  return {
-    id: Date.now(),
-    context: user_input,
-    isComplete: true,
-    category: ["Personal"],
-  };
-}
+// export const ACTIONS = {
+//   ADD_TODO: "add-todo",
+//   DELETE_TODO: "delete-todo",
+//   TOGGLE_AS_DONE: "done-todo",
+//   ADD_TO_CATEGORY_TODO: "add-to-category",
+//   EDIT_TODO: "edit-todo",
+//   CLEAR_TODO: "clear-todo",
+// };
 
 
+// function reducer(todos, action) {
+//   switch (action.type) {
 
-export const dispatchContext = createContext(null)
+//     case ACTIONS.ADD_TODO:
+//       return [...todos, newTodo(action.payload.inputText)];
+//       break;
+//     case ACTIONS.TOGGLE_AS_DONE:
+//       return todos.map((todo) => {
+//         if (action.payload.id === todo.id) {
+//           return { ...todo, isComplete: !todo.isComplete };
+//         }
+//         return todo;
+//       });
+//       break;
+//     case ACTIONS.DELETE_TODO:
+//       return todos.filter((todo) => {
+//         return todo.id !== action.payload.id;
+//       });
+//       break;
+//       case ACTIONS.CLEAR_TODO:
+//         return []
+//       break;
+//       case ACTIONS.EDIT_TODO:
+//         console.log(action.payload.id)
+//       break;
+//         break;
+//   }
+// }
+
+// function newTodo(user_input) {
+//   return {
+//     id: Date.now(),
+//     context: user_input,
+//     isComplete: true,
+//     category: ["Personal"],
+//   };
+// }
 
 
 
-function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
+// export const dispatchContext = createContext(null)
+
+// function App() {
+//   const [todos, dispatch] = useReducer(reducer, []);
    
 
 
-  function handleCreateTodo(inputText) {
-    dispatch({ type: ACTIONS.ADD_TODO, payload: { inputText } });
-  }
+//   function handleCreateTodo(inputText) {
+//     dispatch({ type: ACTIONS.ADD_TODO, payload: { inputText } });
+//   }
 
-  console.log(todos);
+//   console.log(todos);
 
+//   return (
+//     <>
+//     <dispatchContext.Provider value={dispatch}>
+//             <InputBar onCreate={handleCreateTodo}/>
+//       {todos && <TodosList todos={todos}/>}
+//     </dispatchContext.Provider>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+function App() {
   return (
     <>
-    <dispatchContext.Provider value={dispatch}>
-            <InputBar onCreate={handleCreateTodo}/>
-      {todos && <TodosList todos={todos}/>}
-    </dispatchContext.Provider>
+    <InputBar/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/InputBar.jsx
+++ src/Components/InputBar.jsx
@@ -1,34 +1,75 @@
 import { useState,useContext } from 'react'
 import './Styles/Components.scss'
-import { ACTIONS } from '../App'
-import { dispatchContext } from '../App'
-function InputBar({onCreate}) {
+// import { ACTIONS } from '../App'
+// import { dispatchContext } from '../App'
+// function InputBar({onCreate}) {
 
-    const [input,setInput] =useState('')
-    const dispatch = useContext(dispatchContext)
+//     const [input,setInput] =useState('')
+//     const dispatch = useContext(dispatchContext)
 
-    const handleInput = (event)=>{
-        console.log(event.target.value)
-        setInput(event.target.value)    
+//     const handleInput = (event)=>{
+//         console.log(event.target.value)
+//         setInput(event.target.value)    
+//     }
+
+//     const handleSubmit = (event) => {
+//     event.preventDefault();
+//     onCreate(input)
+//     setInput('')
+//   };
+
+//     return (
+//         <div className='input-container'>
+//             <button>Create Task</button>
+//             <form onSubmit={handleSubmit} >
+//                 <input type="text"  onChange={handleInput} value={input} placeholder='Create a note...' />
+//             </form>
+//             <section>
+//                 <button className='add-btn' onClick={handleSubmit}>Add Todo</button>
+//                 <button className='clr-btn' onClick={()=>{dispatch({type:ACTIONS.CLEAR_TODO})}}>Clear Todo</button>
+//             </section>
+//         </div>
+//     )
+// }
+
+function InputBar() {
+
+    const [isActive,setIsActive] = useState(false)
+    const handleCreate =()=>{
+        setIsActive(!isActive)
     }
 
-    const handleSubmit = (event) => {
-    event.preventDefault();
-    onCreate(input)
-    setInput('')
-  };
 
     return (
-        <div className='input-container'>
-            <form onSubmit={handleSubmit} >
-                <input type="text"  onChange={handleInput} value={input} placeholder='Create a note...' />
-            </form>
-            <section>
-                <button className='add-btn' onClick={handleSubmit}>Add Todo</button>
-                <button className='clr-btn' onClick={()=>{dispatch({type:ACTIONS.CLEAR_TODO})}}>Clear Todo</button>
-            </section>
+        <div className='input-bar-container'>
+            <button onClick={handleCreate} className={`task-create-btn ${isActive == true && 'hide'}`}>New Task +</button>
+             {isActive && <TodoForm/>}
         </div>
     )
 }
 
+function TodoForm(){
+const [input,setInput] =useState('')
+console.log(input)
+return (
+     <form action="">
+        <textarea className='todo__input' onChange={(event)=>{setInput(event.target.value)}} defaultValue={input}  name="" id=""></textarea>
+
+        <select name="categories" id="categories">
+    <option value="" disabled selected hidden>Priority Level</option>
+    <option value="Personal">Personal</option>
+    <option value="Home">Home</option>
+    <option value="Work">Work</option>
+    <option value="Others">Others</option>
+        </select>
+
+        <select name="urgency" id="urgency">
+            <option value="" disabled selected hidden>Priority Level</option>
+    <option value="low">Can Wait</option>
+    <option value="normal">Needs Attention</option>
+    <option value="urgent">Handle Immediately</option>
+        </select>
+                </form>
+)
+}
 export default InputBar
